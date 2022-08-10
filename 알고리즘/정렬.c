5e03b1bc117a0fc729f1177e4e22bb6cc1609f95
styoung0507 <styoung0507@gmail.com>
2022-08-11 02:56:24 +0900

    정렬 수정

--- 알고리즘/정렬.c
+++ 알고리즘/정렬.c
@@ -11,13 +11,16 @@ int Sequential(){
 	int i, j;
 	scanf("%d", &n);
 	for(i=0;i<n;i++) scanf("%d", &arr[i]);
-	for(i=0; i<n; i++)
-        for(j=i+1; j<n; j++)
-            if(arr[i] > arr[j]) {
+	for(i=0; i<n; i++){
+		for(j=i+1; j<n; j++){
+        	if(arr[i] > arr[j]) {
                 temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
+		}
+	}
+            
     ShowInfo();
 }
 //
@@ -28,12 +31,15 @@ int Bubble(){
 	scanf("%d", &n);
 	for(i=0;i<n;i++) scanf("%d",&arr[i]);
 	for(i=0; i<n-1; i++)
-        for(j=0; j<n-1-i; j++)
-            if(arr[j] > arr[j+1]) {
+	{
+		for(j=0; j<n-1-i; j++){
+        	if(arr[j] > arr[j+1]) {
                 temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
             }
+		}
+	} 
 
     ShowInfo();
 }
