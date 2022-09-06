6ed27fecbeffd9af31b2cfe6a795cf32e5f2de90
styoung0507 <styoung0507@gmail.com>
2022-09-07 01:55:58 +0900

    백준 10815 xx

--- 백준/10815.c
+++ 백준/10815.c
@@ -1,36 +1,24 @@
 #include<stdio.h>
-#include<stdlib.h>
 int arr[20000000];
-void Search(int a, int n){
+int Search(int m, int a){
 	int i;
-	for(i=0;i<n;i++){
+	for(i=0;i<m;i++){
 		if(arr[i] == a){
-			printf("1 ");
-			return;
+			return 1;
 		}
 	}
-	printf("0 ");
-	return;
-}
-int compare(const int *a, const int *b){
-	if(*a < *b)
-		return -1;
-	else if(*a>*b)
-		return 1;
-	else return 0;
+	return 0;
 }
 int main(){
-	int n,m,i,a;
+	int n, m, i, a;
 	scanf("%d", &n);
 	for(i=0;i<n;i++){
 		scanf("%d", &arr[i]);
 	}
-	qsort(arr, n, sizeof(int), compare);
 	scanf("%d", &m);
 	for(i=0;i<m;i++){
 		scanf("%d", &a);
-		Search(a,n);
+		printf("%d ",Search(n, a));
 	}
 	return 0;
 }
-
