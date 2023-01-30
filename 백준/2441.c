19930f592d78f8ac7668e0846a96c343fc72d391
sty0507 <styoung0507@gmail.com>
2023-01-31 01:43:06 +0900

    백준 2441

---
+++ 백준/2441.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	int n, i, j, k;
+	scanf("%d", &n);
+	for(i=0; i<n;i++){
+		for(j=0;j<i;j++){
+			printf(" ");
+		}
+		for(k=1;k<=n-i;k++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
