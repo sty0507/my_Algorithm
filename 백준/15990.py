28088b8ae3e43500c5c203afd07d0826bf51338f
sty0507 <styoung0507@gmail.com>
2023-05-03 09:42:10 +0900

    백준 15990

---
+++ 백준/15990.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+t = int(input())
+dp = [[0]* 3 for _ in range(100001)]
+dp[0] = [1,0,0]
+dp[1] = [0,1,0]
+dp[2] = [1,1,1]
+for i in range(3, 100001):
+    dp[i][0] = (dp[i-1][1] + dp[i-1][2])%1000000009
+    dp[i][1] = (dp[i-2][0] + dp[i-2][2])%1000000009
+    dp[i][2] = (dp[i-3][0] + dp[i-3][1])%1000000009
+for _ in range(t):
+    n = int(input())
+    print(sum(dp[n-1]) % 1000000009)
