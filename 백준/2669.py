27f73b4cc1af70350afcfef6df911fe3e166211f
sty0507 <styoung0507@gmail.com>
2023-05-11 11:52:40 +0900

    백준 2669

---
+++ 백준/2669.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+arr = [[0] * 100 for _ in range(100)]
+
+for _ in range(4):
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            arr[i][j] += 1
+
+result = 0
+l = len(arr)
+for i in range(l):
+    for j in range(l):
+        if arr[i][j] > 0 :
+            result += 1
+
+print(result)
