8d6e8fe1d6466d7afd1dfe633f21963adf741682
sty0507 <styoung0507@gmail.com>
2024-02-09 20:20:45 +0900

    [프로그래머스] 수박수박수박수박수박수?

---
+++ 프로그래머스/level1/수박수박수박수박수박수?.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = ''
+    for i in range(1, n+1):
+        if i % 2 == 0:
+            answer += "박"
+        else:
+            answer += "수"
+    return answer
+
+def solution(n):
+    str = "수박"*n
+    return str[:n]
