d8dd7f5993c924db23240606d10eae23d6637561
sty0507 <styoung0507@gmail.com>
2024-08-19 17:35:08 +0900

    [백준] 11047

---
+++ 백준/11047.py
@@ -0,0 +1,19 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().rstrip().split())
+
+cost = []
+result = 0
+
+for _ in range(n):
+    x = int(sys.stdin.readline().rstrip())
+    if x <= k:
+        cost.append(x)
+cost.sort(reverse=True)
+for i in range(len(cost)):
+    if k == 0:
+        break
+    result += int(k / cost[i])
+    k %= cost[i]
+
+print(result)
