16f928f8fb5e87c6ff915342f395fb5d01b9ccd7
sty0507 <styoung0507@gmail.com>
2024-08-30 00:21:46 +0900

    [백준] 2579

--- 백준/2579.py
+++ 백준/2579.py
@@ -1,3 +1,4 @@
+"""
 n = int(input())
 dp = [0] * 100100
 sta = [0] * 100100
@@ -12,4 +13,22 @@ else:
     for i in range(2, n+1):
         dp[i] = max(dp[i-2], dp[i-3] + sta[i-1]) + sta[i]
 
+    print(dp[n])
+"""
+
+import sys
+n = int(sys.stdin.readline())
+dp = [0] * (n + 1)
+stairs = [0] * (n + 1)
+
+for i in range(n):
+    stairs[i] = int(sys.stdin.readline())
+
+if n <= 2:
+    print(sum(stairs))
+else:
+    dp[0] = stairs[0]
+    dp[1] = dp[0] + stairs[1]
+    for i in range(2, n+1):
+        dp[i] = max(dp[i-2], dp[i-3] + stairs[i-1]) + stairs[i]
     print(dp[n])
