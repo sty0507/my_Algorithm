2880ecdd513776b3459a39d4ecd8e5e5c0c71376
sty0507 <styoung0507@gmail.com>
2024-10-04 21:35:40 +0900

    [백준] 11726

--- 백준/11726.py
+++ 백준/11726.py
@@ -1,3 +1,4 @@
+"""
 n = int(input())
 dp = [0] * 10000
 dp[1] = 1
@@ -12,6 +13,21 @@ else :
         dp[i] = dp[i-2] + dp[i-1]
     result = dp[i]
 
-
-
 print("%d"% (result % 10007))
+"""
+
+# 최적화 버전
+import sys
+n = int(sys.stdin.readline())
+dp = [0] * (n + 2)
+
+dp[1] = 1
+dp[2] = 2
+
+if n <= 2:
+    print(dp[n] % 10007)
+else:
+    for i in range(3, n+1):
+        dp[i] = dp[i-2] + dp[i-1]
+
+    print(dp[n] % 10007)
